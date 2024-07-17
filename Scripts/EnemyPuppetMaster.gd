9a6da10661473b97b22a3624d7681f166377367d
mathew-tran <mathew.tran.dev@gmail.com>
2024-07-16 21:17:37 -0700

    rough idea of how it could work with a state machine

--- Scripts/EnemyPuppetMaster.gd
+++ Scripts/EnemyPuppetMaster.gd
@@ -5,15 +5,53 @@ var Enemies
 var PlayerReference : Player
 @onready var FormTimer = $FormTimer
 
+enum PUPPET_STATE {
+	INIT,
+	FORM,
+	IN_PROCESS,
+	FORMED,
+	ATTACK_ALL,
+}
+var CurrentState = PUPPET_STATE.INIT
+
 func _ready():
 	Enemies = Helper.GetEnemies()
 	PlayerReference = Helper.GetPlayer()
 
+	CurrentState = PUPPET_STATE.FORM
+
+func _process(delta):
+	match CurrentState:
+		PUPPET_STATE.INIT:
+			pass
+		PUPPET_STATE.FORM:
+			Form()
+		PUPPET_STATE.IN_PROCESS:
+			pass
+		PUPPET_STATE.FORMED:
+			Formed()
+		PUPPET_STATE.ATTACK_ALL:
+			AttackAll()
+
+func Form():
+	CurrentState = PUPPET_STATE.IN_PROCESS
 	for enemy in Enemies.get_children():
 		enemy.Command_MoveToPosition(PlayerReference.global_position)
 
 	FormTimer.wait_time = Enemies.get_child_count() * .5
 	FormTimer.start()
 	await FormTimer.timeout
+	CurrentState = PUPPET_STATE.FORMED
+
+func Formed():
+	CurrentState = PUPPET_STATE.ATTACK_ALL
+	pass
+
+func AttackAll():
+	CurrentState = PUPPET_STATE.IN_PROCESS
 	for enemy in Enemies.get_children():
-		enemy.Command_Attack(PlayerReference.global_position)
+		enemy.Command_MoveToPosition(PlayerReference.global_position)
+	FormTimer.wait_time = Enemies.get_child_count() * .5
+	FormTimer.start()
+	await FormTimer.timeout
+	CurrentState = PUPPET_STATE.FORM
